81b943beb451d705854374cff41d7f7bac3ee700
Luís Mourão <luis@phygitall.rio>
2022-10-16 11:08:47 -0300

    test local cmd

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 export async function electionIntegration(event) {
+  //serverless invoke local -f electionIntegration -p events/test.json
   return {
     message: "Go Serverless v3! Your function executed successfully!",
     input: event,
